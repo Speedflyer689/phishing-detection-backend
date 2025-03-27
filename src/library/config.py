4379b0504086c51a190dd16b0e20ff83ec332f3e
Speedflyer689 <speedflyer67@gmail.com>
2025-03-27 22:39:04 +0530

    Add machine learning models and update phishing detection inputs and computers

--- src/library/config.py
+++ src/library/config.py
@@ -1,2 +1,6 @@
+import os
+from .model_managers import PhishingEmailDetector, PhishingUrlDetector
+
 class Config:
-    ...
+    PHISHING_EMAIL_DETECTOR: PhishingEmailDetector
+    PHISHING_URL_DETECTOR: PhishingUrlDetector
--- src/resources/modules/phishing/is_phishing_email/compute.py
+++ src/resources/modules/phishing/is_phishing_email/compute.py
@@ -1,16 +1,15 @@
 from .input import PhishingEmailInput
 from library.api import APIOutput, BaseComputer
-
+from library.config import Config
+from library.logger import LOGGER
 
 class PhishingEmailComputer(BaseComputer):
     def compute(self, api_input: PhishingEmailInput) -> APIOutput:
-        
-        # Static data
-        # @Priya843, Fix this once you are done
-        
-        is_phishing = True # Calculate this value using ML
-        
+        LOGGER.info(f"Detecting if email is phishing or not.")
+        LOGGER.info(f"Data: {api_input.model_dump()}")
+        is_phishing = Config.PHISHING_EMAIL_DETECTOR.predict(api_input.emailText)
         data = {
             "is_phishing": is_phishing
         }
+        LOGGER.info(f"Output: {data}")
         return APIOutput.success(data=data, message="Phishing email analysed")
